600ddeb1dff38eec367f64316f76fbec2821c420
Misiek95 <michal.wozniak16@gmail.com>
2020-01-12 15:11:29 +0100

    symulation

--- GLCM/GLCM/Form1.cs
+++ GLCM/GLCM/Form1.cs
@@ -11,154 +11,122 @@ using System.Windows.Forms;
 
 namespace GLCM
 {
-	public partial class Form1 : Form
-	{
-		public const int MatrixSize = 4;
+    public partial class Form1 : Form
+    {
+        int delllay = 5;
+        private int cellSize = 40;
+        private int startMatrixPosX = 20;
+        private int startMatrixPosY = 20;
+        int MatrixSize;
+        List<List<int>> startMatrix = new List<List<int>>();
+        Button[,] tabBtn;
+        Button[,] changeMatrix;
+        Button[,] transponedMatrix;
+        Button[,] addedMatrix;
+        Button[,] finalMatrix;
 
-		private int cellSize = 20;
-		private int startMatrixPosX = 20;
-		private int startMatrixPosY = 20;
+        Button sum;
+        Button sumFinal;
+        
 
+        public Form1()
+        {
+            InitializeComponent();
+            InitalizeMatrix(startMatrix);
+            Start();
+           
+       }
 
-		int[,] startMatrix = new int[MatrixSize, MatrixSize];
-		int[,] changeMatrix;
-		int[,] transponedMatrix;
-		int[,] addedMatrix;
-
-		double[,] finalMatrix;
-
-		private int sum;
-		private double sumFinal;
-
-		private double[,] devidedMatrix = new double[MatrixSize, MatrixSize];
-		private Button[,] tabBtn  = new Button[MatrixSize, MatrixSize];
-
-		public Form1()
-		{
-			InitializeComponent();
-			InitalizeMatrix(startMatrix);
-			Start();
-			countOccurance(startMatrix, changeMatrix, transponedMatrix);
-			viewMatrix(changeMatrix, 120, 20);
-			viewMatrix(transponedMatrix, 220, 20);
-			sumMatrix(changeMatrix, transponedMatrix, addedMatrix, ref sum);
-			viewMatrix(addedMatrix, 320, 20);
-			//todo: display sum
-			viewText(sum.ToString(), 320, 120);
-			devideMatrix(addedMatrix, sum, finalMatrix, ref sumFinal);
-			viewMatrix(finalMatrix, 420, 20);
-			//todo: display sumFinal
-			viewText(sumFinal.ToString(), 420, 120);
-
-		}
-
-		/// <summary>
+        /// <summary>
 		/// Renders matrix on first step
 		/// </summary>
 		private void Start()
-		{
-			for (int i = 0; i < MatrixSize; i++)
-			{
-				for (int j = 0; j < MatrixSize; j++)
-				{
-					tabBtn[i, j] = new Button();
-					tabBtn[i, j].Enabled = false;
-					tabBtn[i, j].BackColor = Color.White;
-					tabBtn[i, j].Location = new Point(startMatrixPosX + cellSize * i, startMatrixPosY + cellSize * j);
-					tabBtn[i, j].Size = new Size(cellSize, cellSize);
-					tabBtn[i, j].Visible = true;
-					tabBtn[i, j].Text = startMatrix[i, j].ToString();
-					this.Controls.Add(tabBtn[i, j]);
-				}
-			}
+        {
+            int sizeListY = startMatrix.Count;
+            int sizeListX = 0;
+            if (sizeListY > 0)
+            {
+                sizeListX = startMatrix[0].Count;
+            }
 
-			//after we have the needed 
-			int newMatrixSize = GetMaxValue(startMatrix);
-			changeMatrix = new int[newMatrixSize, newMatrixSize];
-			transponedMatrix = new int[newMatrixSize, newMatrixSize];
-			addedMatrix = new int[newMatrixSize, newMatrixSize];
-			finalMatrix = new double[newMatrixSize, newMatrixSize];
-		}
+            Console.WriteLine(sizeListX + "  " + sizeListY);
+            tabBtn = new Button[sizeListX, sizeListY];
+            for (int i = 0; i < sizeListX; i++)
+            {
+                for (int j = 0; j < sizeListY; j++)
+                {
+                    tabBtn[i, j] = new Button();
+                    tabBtn[i, j].Enabled = false;
+                    tabBtn[i, j].BackColor = Color.White;
+                    tabBtn[i, j].Location = new Point(startMatrixPosX + cellSize * i, startMatrixPosY + cellSize * j);
+                    tabBtn[i, j].Size = new Size(cellSize, cellSize);
+                    tabBtn[i, j].Visible = true;
+                    tabBtn[i, j].Text = startMatrix[j][i].ToString();
+                    this.Controls.Add(tabBtn[i, j]);
+                }
+            }
 
-		private void viewMatrix(int[,] matrix, int posX, int posY)
-		{
-			int size = GetMaxValue(startMatrix);
-		Button[,] tabBtn2 = new Button[size, size];
+        }
 
-			for (int i = 0; i < size; i++)
-			{
-				for (int j = 0; j < size; j++)
-				{
-					tabBtn2[i, j] = new Button();
-					tabBtn2[i, j].Enabled = false;
-					tabBtn2[i, j].BackColor = Color.White;
-					tabBtn2[i, j].Location = new Point(posX + cellSize * i, posY + cellSize * j);
-					tabBtn2[i, j].Size = new Size(cellSize, cellSize);
-					tabBtn2[i, j].Visible = true;
-					tabBtn2[i, j].Text = matrix[i, j].ToString();
-					this.Controls.Add(tabBtn2[i, j]);
-				}
-			}
-		}
+        private void viewMatrix(Button[,] tabBtn2, int posX, int posY)
+        {
+            int size = GetMaxValue(startMatrix) ;
 
-		private void viewMatrix(double[,] matrix, int posX, int posY)
-		{
-			int size = GetMaxValue(startMatrix);
-			Button[,] tabBtn2 = new Button[size, size];
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    tabBtn2[i, j] = new Button();
+                    tabBtn2[i, j].Enabled = false;
+                    tabBtn2[i, j].BackColor = Color.White;
+                    tabBtn2[i, j].Location = new Point(posX + cellSize * i, posY + cellSize * j);
+                    tabBtn2[i, j].Size = new Size(cellSize, cellSize);
+                    tabBtn2[i, j].Visible = true;
+                    tabBtn2[i, j].Text = "0";
+                    this.Controls.Add(tabBtn2[i, j]);
+                    this.Refresh();
+                }
+            }
+        }
 
-			for (int i = 0; i < size; i++)
-			{
-				for (int j = 0; j < size; j++)
-				{
-					tabBtn2[i, j] = new Button();
-					tabBtn2[i, j].Enabled = false;
-					tabBtn2[i, j].BackColor = Color.White;
-					tabBtn2[i, j].Location = new Point(posX + cellSize*3 * i, posY + cellSize * j);
-					tabBtn2[i, j].Size = new Size(cellSize*3, cellSize);
-					tabBtn2[i, j].Visible = true;
-					tabBtn2[i, j].Text = matrix[i, j].ToString();
-					this.Controls.Add(tabBtn2[i, j]);
-				}
-			}
-		}
+        private void viewText(ref Button tabBtn, string text, int posX, int posY)
+        {
+            int size = GetMaxValue(startMatrix);
 
-		private void viewText(string text, int posX, int posY)
-		{
-			int size = GetMaxValue(startMatrix);
+            tabBtn = new Button();
+            tabBtn.Enabled = false;
+            tabBtn.BackColor = Color.White;
+            tabBtn.Location = new Point(posX + cellSize, posY + cellSize);
+            tabBtn.Size = new Size(cellSize * 2, cellSize);
+            tabBtn.Visible = true;
+            tabBtn.Text = text;
+            this.Controls.Add(tabBtn);
+        }
 
-					Button tabBtn = new Button();
-					tabBtn.Enabled = false;
-					tabBtn.BackColor = Color.White;
-					tabBtn.Location = new Point(posX + cellSize, posY + cellSize);
-					tabBtn.Size = new Size(cellSize*2, cellSize);
-					tabBtn.Visible = true;
-					tabBtn.Text = text;
-					this.Controls.Add(tabBtn);
-		}
-
-		private void InitalizeMatrix(int[,] matrix)
-		{
+        private void InitalizeMatrix(List<List<int>> matrix)
+        {
             try
             {   // Open the text file using a stream reader.
                 using (StreamReader sr = new StreamReader("exampleMatrix.txt"))
                 {
-                    bool isInitializedMatrix = false;
+
                     string[] tmp;
+                    List<int> tmpList;
+                    int i = 0;
                     String line;
                     // Read the stream to a string, and write the string to the console.
-                    for (int i = 0; i < MatrixSize; i++)
+                    while (!sr.EndOfStream)
                     {
                         line = sr.ReadLine();
                         tmp = line.Split('\t');
-                        if (isInitializedMatrix)
+                        tmpList = new List<int>();
+                        for (int j = 0; j < tmp.Length; j++)
                         {
-                            startMatrix = new int[tmp.Length, tmp.Length];
-                            isInitializedMatrix = true;
+                            Console.WriteLine(j);
+                            tmpList.Add(int.Parse(tmp[j]));
                         }
-                        for (int j = 0; j < MatrixSize; j++)
-                        {
-                            matrix[j, i] = int.Parse(tmp[j]);
-                        }
+                        matrix.Add(tmpList);
+                        i++;
                     }
                 }
             }
@@ -167,89 +135,213 @@ namespace GLCM
                 Console.WriteLine("The file could not be read:");
                 Console.WriteLine(e.Message);
             }
-          
-		}
-		/// <summary>
+            MatrixSize = GetMaxValue(startMatrix);
+            changeMatrix = new Button[MatrixSize, MatrixSize];
+            transponedMatrix = new Button[MatrixSize, MatrixSize];
+            addedMatrix = new Button[MatrixSize, MatrixSize];
+            finalMatrix = new Button[MatrixSize, MatrixSize];
+        }
+
+        /// <summary>
 		/// Gets the maximum value of matrix
 		/// </summary>
 		/// <param name="matrix">The Matrix</param>
 		/// <returns>Maximum Value</returns>
-		private int GetMaxValue(int[,] matrix)
-		{
-			int max = 0;
-			for (int i = 0; i < MatrixSize; i++)
-			{
-				for (int j = 0; j < MatrixSize; j++)
-				{
-					if (matrix[i, j] > max)
-					{
-						max = matrix[i, j];
-					}
-				}
-			}
-			return max+1;
-		}
+		private int GetMaxValue(List<List<int>> matrix)
+        {
+            int sizeListY = startMatrix.Count;
+            int sizeListX = 0;
+            if (sizeListY > 0)
+            {
+                sizeListX = startMatrix[0].Count;
+            }
+            int max = 0;
+            for (int i = 0; i < sizeListY; i++)
+            {
+                for (int j = 0; j < sizeListX; j++)
+                {
+                    if (matrix[i][j] > max)
+                    {
+                        max = matrix[i][j];
 
-		private void countOccurance(int[,] inputMatrix, int[,] outputMatrix, int[,] transpozedOutputMatrix)
-		{
-			for (int i = 0; i < MatrixSize; i++)
-			{
-				for (int j = 0; j < MatrixSize; j++)
-				{
-					outputMatrix[i,j] =  SearchForOccurance(inputMatrix, j, i);
-					transpozedOutputMatrix[i, j] = SearchForOccurance(inputMatrix, i, j);
+                        Console.WriteLine(i + ":" + j);
+                    }
+                }
+            }
+            Console.WriteLine(max);
+            return max + 1;
+        }
 
-				}
-			}
-		}
+        private void countOccurance(Button[,] inputMatrix, Button[,] outputMatrix)
+        {
+            for (int i = 0; i < MatrixSize; i++)
+            {
+                for (int j = 0; j < MatrixSize; j++)
+                { 
+                    outputMatrix[i, j].BackColor = Color.Gray;
+                    outputMatrix[i, j].Text = SearchForOccurance(inputMatrix, j, i).ToString();
+                   this.Refresh();
+                    System.Threading.Thread.Sleep(delllay);
+                    outputMatrix[i, j].BackColor = Color.White;
+                    
+                }
+            }
+           
+        }
 
-		private int SearchForOccurance(int[,] inputMatrix, int x, int y)
-		{
-			int count = 0;
-			for (int i = 0; i < MatrixSize-1; i++)
-			{
-				for (int j = 0; j < MatrixSize; j++)
-				{
-					if (inputMatrix[i,j] == x)
-					{
-						if (inputMatrix[i+1, j] == y)
-						{
-							count++;
-						}
-					}
-				}
-			}
-			return count;
-		}
+        private int SearchForOccurance(Button[,] inputMatrix, int x, int y)
+        {
+            int count = 0;
+            Console.WriteLine("X=" + inputMatrix.GetLength(0) + " Y=" + inputMatrix.GetLength(1));
 
-		private void sumMatrix(int[,] input1, int[,]input2, int[,]output, ref int sum)
-		{
-			int size = GetMaxValue(startMatrix);
-			sum = 0;
-			for (int i = 0; i < size; i++)
-			{
-				for (int j = 0; j < size; j++)
-				{
-					output[i, j] = input1[i, j] + input2[i, j];
-					sum += output[i, j];
-				}
-			}
-		}
+            for (int i = 0; i < inputMatrix.GetLength(0)-1; i++)
+            {
+                for (int j = 0; j < inputMatrix.GetLength(1); j++)
+                {
+                    if (int.Parse(inputMatrix[i, j].Text) == x)
+                    {
+                        if (int.Parse(inputMatrix[i + 1, j].Text) == y)
+                        {
+                            count++;
+                            inputMatrix[i, j].BackColor = Color.Green;
+                            inputMatrix[i+1, j].BackColor = Color.Green;
+                            this.Refresh();
+                            System.Threading.Thread.Sleep(delllay);
+                            inputMatrix[i, j].BackColor = Color.White;
+                            inputMatrix[i+1, j].BackColor = Color.White;
+                        }
+                    }
+                }
+            }
+            return count;
+        }
 
-		private void devideMatrix(int[,] input, int devider, double[,] output, ref double sum)
-		{
-			int size = GetMaxValue(startMatrix);
-			sum = 0;
-			for (int i = 0; i < size; i++)
-			{
-				for (int j = 0; j < size; j++)
-				{
-					output[i, j] = input[i, j] / (double)devider;
-					sum += output[i, j];
-				}
-			}
-		}
+        private void transpozeMatrix(Button[,] inputMatrix, Button[,] transpozedOutputMatrix)
+        {
+            for (int i = 0; i < MatrixSize; i++)
+            {
+                for (int j = 0; j < MatrixSize; j++)
+                {
+                   
+                   
+                    inputMatrix[j, i].BackColor = Color.Yellow;
+                    transponedMatrix[i, j].Text = inputMatrix[j, i].Text;
+                    transponedMatrix[i, j].BackColor = Color.Orange;
+                    this.Refresh();
+                    System.Threading.Thread.Sleep(delllay);
+                    inputMatrix[j, i].BackColor = Color.White;
+                    transponedMatrix[i, j].BackColor = Color.White;
+                    this.Refresh();
 
+                }
+                this.Refresh();
+                System.Threading.Thread.Sleep(delllay);
+            }
+        }
 
-	}
+        private void sumMatrix(Button[,] input1, Button[,] input2, Button[,] output)
+        {
+            int size = GetMaxValue(startMatrix);
+            
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    System.Threading.Thread.Sleep(delllay);
+                    input1[i, j].BackColor = Color.Yellow;
+                    input2[i, j].BackColor = Color.Yellow;
+                    output[i, j].BackColor = Color.Green;
+                    output[i, j].Text = (int.Parse(input1[i, j].Text) + int.Parse(input2[i, j].Text)).ToString();
+                    this.Refresh();
+                    System.Threading.Thread.Sleep(delllay);
+                    input1[i, j].BackColor = Color.White;
+                    input2[i, j].BackColor = Color.White;
+                    output[i, j].BackColor = Color.White;
+                    this.Refresh();
+                }
+            }
+        }
+
+        private void sumCount(Button[,] input, ref Button output)
+        {
+            int size = GetMaxValue(startMatrix);
+            int sum = 0;
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    System.Threading.Thread.Sleep(delllay);
+                    input[i, j].BackColor = Color.Yellow;
+                    output.BackColor = Color.Gray;
+                    sum += int.Parse(input[i, j].Text);
+                    output.Text =  sum.ToString();
+                    this.Refresh();
+                    System.Threading.Thread.Sleep(delllay);
+
+                    input[i, j].BackColor = Color.White;
+                    output.BackColor = Color.White;
+                    this.Refresh();
+                }
+            }
+        }
+
+        private void devideMatrix(Button[,] input, Button devider, Button[,] output)
+        {
+            int size = GetMaxValue(startMatrix);
+            double sum = 0;
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    output[i, j].Text = Math.Round((int.Parse(input[i, j].Text) / double.Parse(devider.Text)), 2).ToString();
+                    sum += double.Parse(output[i, j].Text);
+                }
+            }
+        }
+
+        private void sumFinalCount(Button[,] input, ref Button output)
+        {
+            int size = GetMaxValue(startMatrix);
+            double sum = 0;
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    System.Threading.Thread.Sleep(delllay);
+                    input[i, j].BackColor = Color.Yellow;
+                    output.BackColor = Color.Gray;
+                    sum += double.Parse(input[i, j].Text);
+                    output.Text = sum.ToString();
+                    this.Refresh();
+                    System.Threading.Thread.Sleep(delllay);
+
+                    input[i, j].BackColor = Color.White;
+                    output.BackColor = Color.White;
+                    this.Refresh();
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            viewMatrix(changeMatrix, startMatrixPosX + cellSize * (MatrixSize + 1), startMatrixPosY);
+            countOccurance(tabBtn, changeMatrix);
+            viewMatrix(transponedMatrix, startMatrixPosX + cellSize * (MatrixSize * 2 + 2), startMatrixPosY);
+            transpozeMatrix(changeMatrix, transponedMatrix);
+            viewMatrix(addedMatrix, startMatrixPosX + cellSize * (MatrixSize + 1), startMatrixPosY + cellSize * (MatrixSize + 1));
+            sumMatrix(changeMatrix, transponedMatrix, addedMatrix);
+            
+            //todo: display sum
+            viewText(ref sum, "0", startMatrixPosX + cellSize, startMatrixPosY + cellSize * (MatrixSize + 1));
+            sumCount(addedMatrix,ref sum);
+            viewMatrix(finalMatrix, startMatrixPosX + cellSize * (MatrixSize * 2 + 2), startMatrixPosY + cellSize * (MatrixSize + 1));
+            devideMatrix(addedMatrix, sum, finalMatrix);
+            //todo: display sumFinal
+            viewText(ref sumFinal, "0", startMatrixPosX + cellSize, startMatrixPosY + cellSize * (MatrixSize + 2));
+            sumFinalCount(finalMatrix, ref sumFinal);
+        }
+    }
 }
+
+
+
